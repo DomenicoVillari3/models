33863dd7c5b495cf6909c222b5ad57dcf4ee10f0
DomenicoVillari3 <domenico.villari3@gmail.com>
2024-05-16 17:14:46 +0200

    sm4t

--- TryS4T.py
+++ TryS4T.py
@@ -2,48 +2,72 @@ import torchaudio
 import numpy as np
 import torch
 from transformers import SeamlessM4Tv2Model, AutoProcessor
-import librosa
 from datasets import load_dataset
 import time
 from CalcolaWER import calculate_WER,accuracyFromWER
 from Myplot import my_plot
-
-processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large")
-model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large")
+from WriteMeanToCSV import WriteMeanToCSV,WriteValues
 
 wer_list=[]
 t_list=[]
 acc_list=[]
 
-# let's load an audio sample from an Hindi speech corpus
-dataset = load_dataset("google/fleurs", "it_it", split="test", streaming=True)
-for audio_sample in dataset:
-    transcription=audio_sample["transcription"]
-    audio_sample = audio_sample["audio"]
-    audio = torch.tensor(audio_sample["array"])
-    #print(f"Sampling rate: {audio_sample['sampling_rate']}")
-    audio = torchaudio.functional.resample(audio, orig_freq=audio_sample['sampling_rate'], new_freq=model.config.sampling_rate)
+def LoadModel():
+    processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large")
+    model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large")
+    return model,processor
 
-    #audio, orig_freq =  torchaudio.load("C:\\Users\\domen\\Desktop\\Test_models\\models\\test.wav")
-    #audio =  torchaudio.functional.resample(audio, orig_freq=orig_freq, new_freq=16_000) # must be a 16 kHz waveform array
-
-    # process input
-    audio_inputs = processor(audios=audio, return_tensors="pt")
-
+def use_seamless(model,processor,audio_inputs):
     # generate translation
     start=time.time()
     output_tokens = model.generate(**audio_inputs, tgt_lang="ita", generate_speech=False)
     translated_text_from_audio = processor.decode(output_tokens[0].tolist()[0], skip_special_tokens=True)
     end=time.time()
-    
     elapsed = end-start
-    wer=calculate_WER(transcription,translated_text_from_audio)
-    acc=accuracyFromWER(wer)
 
-    wer_list.append(wer)
-    t_list.append(elapsed)
-    acc_list.append(acc)
+    return translated_text_from_audio,elapsed
 
-    my_plot(wer_list,t_list,"SM4T")
 
-    print(f"Translation from audio: {translated_text_from_audio}")
+def main():
+    model,processor=LoadModel()
+
+    for run in range(20):
+        wer_list = []
+        time_list=[]
+        acc_list = []
+        #CARICAMENTO DEL DATASET
+        dataset = load_dataset("google/fleurs", "it_it", split="test", streaming=True,trust_remote_code=True)
+
+        for audio_sample in dataset:
+            transcription=audio_sample["transcription"]
+            audio_sample = audio_sample["audio"]
+            audio = torch.tensor(audio_sample["array"])
+            #print(f"Sampling rate: {audio_sample['sampling_rate']}")
+            audio = torchaudio.functional.resample(audio, orig_freq=audio_sample['sampling_rate'], new_freq=model.config.sampling_rate)
+
+            # process input
+            audio_inputs = processor(audios=audio, return_tensors="pt")
+
+
+            ipotesi,t=use_seamless(model,processor,audio_inputs)
+    
+
+            wer=calculate_WER(transcription,ipotesi)
+            acc=accuracyFromWER(wer)
+
+            wer_list.append(wer)
+            t_list.append(t)
+            acc_list.append(acc)
+            print("\n ipotesi: {}\ntrascrizione: {} \n".format(ipotesi, transcription))
+
+        #TRASCRIZIONI SU FILE CSV DEI VALORI MEDI 
+        WriteMeanToCSV("whisperMEAN.csv",run,avg_wer=np.mean(wer_list),avg_time=np.mean(time_list),avg_accuracy=np.mean(acc_list)) 
+        WriteValues("whisper.csv",run,wer_l=wer_list,time_l=time_list,accuracy_l=acc_list)     
+
+    my_plot("SM4T.csv","SM4T.csv")
+
+    
+
+
+if __name__ == "__main__":
+    main()
